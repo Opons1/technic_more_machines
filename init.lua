666491dd15327113acf9fe9da49f0820d8c1d44b
Opons1 <tazimc1001@gmail.com>
2025-12-26 11:37:50 -0800

    y

--- init.lua
+++ init.lua
@@ -10,23 +10,23 @@ if core.get_modpath("tubelib") then
     dofile(modpath.."/LV/lv_tubelib_repairer.lua")
 end
 --dofile(modpath.."/LV/lv_switcher.lua") --don't work
-
+--I need to make compatible with technic plus
 --MV
 --distiller recipes first, then distiller
 --does not load without farming redo
 --please tell me if there is a better way to detect farming redo
-if farming.node_sound_defaults then
-dofile(modpath.."/MV/mv_distiller_recipes.lua")
-dofile(modpath.."/MV/mv_distiller.lua")
-end
+--if farming.node_sound_defaults then
+--dofile(modpath.."/MV/mv_distiller_recipes.lua")
+--dofile(modpath.."/MV/mv_distiller.lua")
+--end
 dofile(modpath.."/MV/mv_riteg.lua")
 dofile(modpath.."/MV/mv_spotlight.lua")
 dofile(modpath.."/MV/mv_geothermal.lua")
 dofile(modpath.."/MV/mv_oxygen_generator.lua")
 
 --HV
-dofile(modpath.."/HV/hv_transmutator.lua")
-dofile(modpath.."/HV/hv_transmutator_recipes.lua")
+--dofile(modpath.."/HV/hv_transmutator.lua")
+--dofile(modpath.."/HV/hv_transmutator_recipes.lua")
 dofile(modpath.."/HV/hv_furnace.lua")
 dofile(modpath.."/HV/hv_laser_door.lua")
 dofile(modpath.."/HV/hv_terraformer.lua")
